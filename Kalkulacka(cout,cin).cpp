78f53c91e630c13621fe62c3c3a4d358ab977859
Agrossik <91050230+Agrossik@users.noreply.github.com>
2021-10-11 16:58:17 +0200

    Add files via upload

---
+++ Kalkulacka(cout,cin).cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	int x, y;
+	int sucet;
+	int odcitanie;
+	int delenie;
+	int nasobenie;
+	cout << "Zadaj prve cislo:";
+	cin >> x;
+	cout << "Zadaj druhe cislo:";
+	cin >> y;
+	sucet = x + y;
+	odcitanie = x - y;
+	nasobenie = x * y;
+	delenie = x / y;
+	cout << "Sucet je :" << sucet << endl;
+	cout << "Rozdiel je :" << odcitanie << endl;
+	cout << "Sucin je :" << nasobenie << endl;
+	cout << "Podiel je:" << delenie << endl;
+	
+	
+}
