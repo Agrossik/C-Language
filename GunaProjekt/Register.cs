9c6db31c63b47b9963ba80c08e297d39cd0bf942
Agrossik <91050230+Agrossik@users.noreply.github.com>
2022-06-16 22:26:35 +0200

    Add files via upload

---
+++ GunaProjekt/Register.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GunaProjekt
+{
+    public partial class Register : Form
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void guna2ControlBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void guna2PictureBox1_Click(object sender, EventArgs e)
+        {
+            Form1 _load = new Form1();
+            _load.Show();
+            this.Hide();
+        }
+
+        private void Label_Click(object sender, EventArgs e)
+        {
+            Form1 _load = new Form1();
+            _load.Show();
+            this.Hide();
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            Menu _load = new Menu();
+            _load.Show();
+            this.Hide();
+        }
+    }
+}
